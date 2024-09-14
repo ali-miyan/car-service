a36444df1b0179274b578227cd48faf95d1a609a
ali-miyan <alimiyan1112@gmail.com>
2024-09-14 16:54:59 +0530

    loader added

--- frontend/src/components/common/Loader.tsx
+++ frontend/src/components/common/Loader.tsx
@@ -2,7 +2,7 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white transition-opacity duration-1000">
       <img
-        src={"../../../../public/assets/car-dealer-loader-gif.gif"}
+        src={"/assets/car-dealer-loader-gif.gif"}
         alt="Loading"
         className="w-36 h-36"
       />
