e9f17d3ade08590b0ba4b134aab30057877be8cf
ALI <136926789+ali-miyan@users.noreply.github.com>
2024-09-28 19:04:36 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -5,29 +5,29 @@ cd frontend
 npm run dev &
 cd ..
 
-# echo "Starting user service..."
-# cd backend/userService
-# npm start &
-# cd ../..
+echo "Starting user service..."
+cd backend/userService
+npm start &
+cd ../..
 
-# echo "Starting company service..."
-# cd backend/companyServive
-# npm start &
-# cd ../..
+echo "Starting company service..."
+cd backend/companyServive
+npm start &
+cd ../..
 
 echo "Starting admin service..."
 cd backend/adminService
 npm start &
 cd ../..
 
-# echo "Starting booking service..."
-# cd backend/bookingService
-# npm start &
-# cd ../..
+echo "Starting booking service..."
+cd backend/bookingService
+npm start &
+cd ../..
 
-# echo "Starting chat service..."
-# cd backend/chatService
-# npm start &
-# cd ../..
+echo "Starting chat service..."
+cd backend/chatService
+npm start &
+cd ../..
 
 wait
